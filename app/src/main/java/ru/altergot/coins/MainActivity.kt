ca7ac0e658c30da17979dd22ec5b03c0ef9d9131
Alexey Baranov <abitty@ya.ru>
2017-12-20 00:32:31 +0300

    toolbar, still has issues

--- app/src/main/java/ru/altergot/coins/MainActivity.kt
+++ app/src/main/java/ru/altergot/coins/MainActivity.kt
@@ -5,15 +5,17 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.provider.AlarmClock.EXTRA_MESSAGE
-import android.view.View
 import android.view.View.OnClickListener
-
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //tSupportActionBar(toolbar)
+        val tb = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(tb)
 /*
         login_button.setOnClickListener {
             // Handler code here.
